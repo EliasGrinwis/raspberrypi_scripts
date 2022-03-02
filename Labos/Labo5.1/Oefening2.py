0315f4706bb42da6a71d74fb3a609eac89d8d721
elias <elias@example.com>
2022-03-02 14:48:26 +0100

    oefening2 completed

---
+++ Labos/Labo5.1/Oefening2.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+def blink(pin1, pin2, pin3):
+    GPIO.setup(pin1, GPIO.OUT)
+    GPIO.setup(pin2, GPIO.OUT)
+    GPIO.setup(pin3, GPIO.OUT)
+    GPIO.output(pin1, 1)
+    GPIO.output(pin2, 1)
+    GPIO.output(pin3, 1)
+    time.sleep(0.5)
+    GPIO.output(pin1, 0)
+    GPIO.output(pin2, 0)
+    GPIO.output(pin3, 0)
+
+    time.sleep(0.5)
+
+
+while True:
+    blink(18, 23, 24)
+
+GPIO.cleanup()
