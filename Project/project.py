f707d3a30c8477401b188767aafff96bf9c8aec5
elias <elias@example.com>
2022-04-03 19:54:15 +0200

    Project

--- Project/project.py
+++ Project/project.py
@@ -1,36 +1,13 @@
-# import RPi.GPIO as GPIO
-# import time
-# GPIO.setmode(GPIO.BOARD)
-# control_pins = [7,11,13,15]
-# for pin in control_pins:
-#   GPIO.setup(pin, GPIO.OUT)
-#   GPIO.output(pin, 0)
-# halfstep_seq = [
-#   [1,0,0,0],
-#   [1,1,0,0],
-#   [0,1,0,0],
-#   [0,1,1,0],
-#   [0,0,1,0],
-#   [0,0,1,1],
-#   [0,0,0,1],
-#   [1,0,0,1]
-# ]
-# for i in range(512):
-#   for halfstep in range(8):
-#     for pin in range(4):
-#       GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-#     time.sleep(0.001)
-# GPIO.cleanup()
-
-
-
 import RPi.GPIO as GPIO
 from datetime import datetime
+from time import sleep
 import time
+ 
 GPIO.setmode(GPIO.BCM)
 
 TRIG = 23 
 ECHO = 24
+
 
 print ("Distance Measurement In Progress")
 
@@ -62,7 +39,27 @@ try:
         distance = round(distance, 2)
         print ("Distance:", distance,"cm")
         print(current_time)
+        if current_time == "18:00:00":
+            print ("Feeding the fishes at", current_time)
+            StepPins = [17,22,5,6]
+            for pin in StepPins:
+                GPIO.setup(pin,GPIO.OUT)
+                GPIO.output(pin, False)
+            Seq = [[1,0,0,0],
+                    [1,1,0,0],
+                    [0,1,0,0],
+                    [0,1,1,0],
+                    [0,0,1,0],
+                    [0,0,1,1],
+                    [0,0,0,1],
+                    [1,0,0,1]]
+            
+            for i in range(512):
+                for Seq2 in range(8):
+                    for pin in range(4):
+                        GPIO.output(StepPins[pin], Seq[Seq2][pin])
+                    time.sleep(0.001)
 
-except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
+except KeyboardInterrupt:
     print("Cleaning up!")
-    gpio.cleanup()
+    GPIO.cleanup()
